63b50813db019a66d5dbd5c21b9d3b32f16d54ef
Milan Filipovic <milan.filipovic@rokolabs.com>
2024-11-06 12:28:33 +0100

    Adding deploy job

--- IdentityApi/obj/Debug/net8.0/IdentityApi.AssemblyInfo.cs
+++ IdentityApi/obj/Debug/net8.0/IdentityApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+025bdb0b03643f185de54459205179cef6f186ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2223aeb7c7010ed4d6d05594d0b322788dd2cf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
